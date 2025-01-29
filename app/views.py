8bc4f6faf91848d673cf1148a75b04e0b8a84443
aditi saudagar <aditisaudagar17@gmail.com>
2025-01-29 14:13:49 +0530

    registration

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+def register(request):
+    if request.method == 'post':
+        pass
+    else:
+        return render(request, 'register.html')
+def login(request):
+    return render(request,'login.html')
+    
